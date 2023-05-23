4f06455e7c4ce3efb238cec560a356e68baaed15
MahmoudShaheem <134424761+MahmoudShaheem@users.noreply.github.com>
2023-05-23 19:47:50 +0300

    Update hi.java

--- src/hi.java
+++ src/hi.java
@@ -2,6 +2,7 @@ public class hi {
     public static void main(String[] args) {
 
         System.out.println("Hello and welcome!");
+         System.out.println("Hello from another account!");
         // modify
         // second modfiy
 }}
